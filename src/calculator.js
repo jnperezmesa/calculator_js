7eb5a995c426b68405aba5b1db0ec70f9298aa98
Javier Nicolás Pérez Mesa <javier@perezmesa.com>
2024-05-24 21:22:37 +0200

    Update aggregate for edge cases

--- src/calculator.js
+++ src/calculator.js
@@ -1,6 +1,11 @@
 class Calculator {
     aggregation(first_number, second_number) {
-        return first_number + second_number;
+
+        if (typeof first_number == "string" || typeof second_number == "string") {
+            return "Error"
+        } else {
+            return first_number + second_number;
+        }
     }
 }
 
