5ab126821685c79b1dff02331510388698b40e7c
Javier Nicolás Pérez Mesa <javier@perezmesa.com>
2024-05-31 20:06:29 +0200

    Add new constants on ui file

--- scripts/ui.js
+++ scripts/ui.js
@@ -1,7 +1,12 @@
 import Calculator from './calculator';
 
-// ============ Variables ============
+// ============ Constants ============
 const CALCULATOR = new Calculator()
+const PREVIOUS = "previous"
+const RESULT = "result"
+
+
+// ============ Variables ============
 let operation = []
 let availableOperations = '='
 
@@ -9,9 +14,9 @@ let availableOperations = '='
 // ============ Functions ============
 function setEqualListener() {
     document.getElementById("=").addEventListener("click", () => {
-        let result = document.getElementById("previous").value
+        let result = document.getElementById(PREVIOUS).value
         if (!Array.from(result).some((letter) => letter === '=')) {
-            operation.push(document.getElementById("result").value)
+            operation.push(document.getElementById(RESULT).value)
             let num1 = parseFloat(operation[0])
             let operationSymbol = operation[1]
             let num2 = parseFloat(operation[2])
@@ -19,26 +24,26 @@ function setEqualListener() {
             switch (operationSymbol) {
                 case "+":
                     result = CALCULATOR.aggregation(num1, num2)
-                    document.getElementById("previous").value = num1 + '+' + num2 + " ="
-                    document.getElementById("result").value = result
+                    document.getElementById(PREVIOUS).value = num1 + '+' + num2 + " ="
+                    document.getElementById(RESULT).value = result
                     operation = [result]
                     break
                 case "-":
                     result = CALCULATOR.subtraction(num1, num2)
-                    document.getElementById("previous").value = num1 + '-' + num2 + " ="
-                    document.getElementById("result").value = result
+                    document.getElementById(PREVIOUS).value = num1 + '-' + num2 + " ="
+                    document.getElementById(RESULT).value = result
                     operation = [result]
                     break
                 case "*":
                     result = CALCULATOR.multiplication(num1, num2)
-                    document.getElementById("previous").value = num1 + '*' + num2 + " ="
-                    document.getElementById("result").value = result
+                    document.getElementById(PREVIOUS).value = num1 + '*' + num2 + " ="
+                    document.getElementById(RESULT).value = result
                     operation = [result]
                     break
                 case "/":
                     result = CALCULATOR.division(num1, num2)
-                    document.getElementById("previous").value = num1 + '/' + num2 + " ="
-                    document.getElementById("result").value = result
+                    document.getElementById(PREVIOUS).value = num1 + '/' + num2 + " ="
+                    document.getElementById(RESULT).value = result
                     operation = [result]
                     break
             }
@@ -49,55 +54,25 @@ function setEqualListener() {
 
 function setRemoveListener() {
     document.getElementById("⌫").addEventListener("click", () => {
-        let result = document.getElementById("result").value
+        let result = document.getElementById(RESULT).value
         if (result.length === 1) {
-            document.getElementById("result").value = 0
+            document.getElementById(RESULT).value = 0
         } else {
-            document.getElementById("result").value = result.slice(0, result.length - 1)
+            document.getElementById(RESULT).value = result.slice(0, result.length - 1)
 
         }
     })
-
-}
-
-function setChangeSymbolListener() {
-    document.getElementById("+/-").addEventListener("click", () => {
-        removePrevious()
-
-        let result = document.getElementById("result").value
-
-        if (result.charAt(0) !== '-') {
-            document.getElementById("result").value = '-' + result
-        } else {
-            document.getElementById("result").value = result.slice(1, result.length)
-        }
-    })
-
 }
 
 function setClearListener() {
     document.getElementById("clear").addEventListener("click", () => {
-        document.getElementById("previous").value = ''
-        document.getElementById("result").value = '0'
+        document.getElementById(PREVIOUS).value = ''
+        document.getElementById(RESULT).value = '0'
         operation = []
     })
 }
 
-function setNumbersListeners() {
-    Array.from(document.getElementsByClassName("number")).forEach((element) => {
-        element.addEventListener("click", (e) => {
-            removePrevious()
 
-            let temp = document.getElementById("result").value
-
-            if ((temp === '0' || temp === '-0') && e.target.id !== '.') {
-                document.getElementById("result").value = temp.replaceAll('0', e.target.id)
-            } else {
-                document.getElementById("result").value += e.target.id
-            }
-        })
-    })
-}
 
 function setOperationListeners() {
     Array.from(document.getElementsByClassName("operation")).forEach((element) => {
@@ -105,7 +80,7 @@ function setOperationListeners() {
         element.addEventListener("click", (e) => {
 
                 if (!operation[0]) {
-                    operation.push(document.getElementById("result").value)
+                    operation.push(document.getElementById(RESULT).value)
                 }
 
                 let num1 = parseFloat(operation[0])
@@ -113,15 +88,15 @@ function setOperationListeners() {
                 switch (e.target.id) {
                     case "√":
                         result = CALCULATOR.squareRoot(num1)
-                        document.getElementById("previous").value = "√" + num1 + " ="
-                        document.getElementById("result").value = result
+                        document.getElementById(PREVIOUS).value = "√" + num1 + " ="
+                        document.getElementById(RESULT).value = result
                         operation = [result]
                         break
 
                     case "e":
                         result = CALCULATOR.exponential(num1)
-                        document.getElementById("previous").value = "e^" + num1 + " ="
-                        document.getElementById("result").value = result
+                        document.getElementById(PREVIOUS).value = "e^" + num1 + " ="
+                        document.getElementById(RESULT).value = result
                         operation = [result]
                         break
 
@@ -136,17 +111,48 @@ function setOperationListeners() {
 }
 
 function replaceResultField(e) {
-    document.getElementById("previous").value = parseFloat(operation[0]) + e.target.id
-    document.getElementById("result").value = 0
+    document.getElementById(PREVIOUS).value = parseFloat(operation[0]) + e.target.id
+    document.getElementById(RESULT).value = 0
     operation.push(e.target.id)
 }
 
+function setChangeSymbolListener() {
+    document.getElementById("+/-").addEventListener("click", () => {
+        removePrevious()
+
+        let result = document.getElementById(RESULT).value
+
+        if (result.charAt(0) !== '-') {
+            document.getElementById(RESULT).value = '-' + result
+        } else {
+            document.getElementById(RESULT).value = result.slice(1, result.length)
+        }
+    })
+
+}
+
+function setNumbersListeners() {
+    Array.from(document.getElementsByClassName("number")).forEach((element) => {
+        element.addEventListener("click", (e) => {
+            removePrevious()
+
+            let temp = document.getElementById(RESULT).value
+
+            if ((temp === '0' || temp === '-0') && e.target.id !== '.') {
+                document.getElementById(RESULT).value = temp.replaceAll('0', e.target.id)
+            } else {
+                document.getElementById(RESULT).value += e.target.id
+            }
+        })
+    })
+}
+
 function removePrevious() {
-    let result = document.getElementById("previous").value
+    let result = document.getElementById(PREVIOUS).value
     if (Array.from(result).some((letter) => Array.from(availableOperations).some((symbol) => symbol === letter))) {
-        document.getElementById("previous").value = ''
+        document.getElementById(PREVIOUS).value = ''
         operation = []
-        document.getElementById("result").value = '0'
+        document.getElementById(RESULT).value = '0'
     }
 }
 
