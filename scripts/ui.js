ea4559d1ff8349c7c1824b367abd3d6358caf778
Javier Nicolás Pérez Mesa <javier@perezmesa.com>
2024-06-01 17:22:27 +0200

    correcting concatenated operations

--- scripts/ui.js
+++ scripts/ui.js
@@ -4,64 +4,121 @@ import Calculator from './calculator';
 const CALCULATOR = new Calculator()
 const PREVIOUS = "previous"
 const RESULT = "result"
+const BASIC_OPERATIONS = ['+', '-', '*', '/']
 
 
 // ============ Variables ============
 let operation = []
-let availableOperations = '='
+//let availableOperations = '='
 let previousDisplay = document.getElementById(PREVIOUS)
 let resultDisplay = document.getElementById(RESULT)
 
 
-// ============ Functions ============
 function setOperationListeners() {
     Array.from(document.getElementsByClassName("operation")).forEach((element) => {
         element.addEventListener("click", (e) => {
-                //Si viene lleno con un valor anterior no se pone otro, que causaria repeticion y que hubiera 4 valores en el array en vez de 3
-                // es para las operaciones encadenadas
-                if (!operation[0]) {
-                    operation.push(resultDisplay.value)
-                }
-
-                // let [num1, operationSymbol, num2] = getOperationData()
-
-                let num1 = parseFloat(operation[0])
+                updateOperation()
+                let num1 = getFirstValue()
                 let result
                 let operationDisplay
                 let finishedOperation = true
+                let operationSymbol = e.target.id
 
-                switch (e.target.id) {
-                    case "√":
-                        result = CALCULATOR.squareRoot(num1)
-                        operationDisplay = "√" + num1
-                        break
 
-                    case "e":
-                        result = CALCULATOR.exponential(num1)
-                        operationDisplay = "e^" + num1
-                        break
+                if (operation.length === 2 && BASIC_OPERATIONS.includes(operationSymbol)) {
+                    launchOperation()
+                } else {
+                    switch (operationSymbol) {
+                        case "√":
+                            result = CALCULATOR.squareRoot(num1)
+                            operationDisplay = "√" + num1
+                            break
 
-                    default:
-                        // aqui se ponen el resto de simbolos en el array operation y en el campo previous (pasa mucha cosa por aqui modificar con cuidado)
-                        result = 0
-                        operationDisplay = parseFloat(operation[0]) + e.target.id
-                        operation.push(e.target.id)
-                        finishedOperation = false
-                        break
+                        case "e":
+                            result = CALCULATOR.exponential(num1)
+                            operationDisplay = "e^" + num1
+                            break
+
+                        default:
+                            result = 0
+                            operationDisplay = operation[0] + operationSymbol
+                            operation.push(operationSymbol)
+                            finishedOperation = false
+                            break
+                    }
+                    updateDisplay(operationDisplay, result, finishedOperation)
                 }
-
-                updateDisplay(operationDisplay, result, finishedOperation)
             }
         )
     })
 }
 
-function updateDisplay(stringOperation, result, addEqual=true) {
+
+// ============ Functions ============
+// function setOperationListeners() {
+//     Array.from(document.getElementsByClassName("operation")).forEach((element) => {
+//         element.addEventListener("click", (e) => {
+//                 updateOperation()
+//                 let num1 = getFirstValue()
+//                 let result
+//                 let operationDisplay
+//                 let finishedOperation = true
+//                 let operationSymbol = e.target.id
+//
+//                 switch (operationSymbol) {
+//                     case "√":
+//                         result = CALCULATOR.squareRoot(num1)
+//                         operationDisplay = "√" + num1
+//                         break
+//
+//                     case "e":
+//                         result = CALCULATOR.exponential(num1)
+//                         operationDisplay = "e^" + num1
+//                         break
+//
+//                     default:  // +, -, *, / -> Add the symbol to the array and the previous display (2 number operations executed on ='s push)
+//                         if (previousDisplay.value.some((valor) => valor in ['+', '-', '*', '/'])) {
+//                             launchOperation()
+//                         } else {
+//
+//                         }
+//                         result = 0
+//                         operationDisplay = operation[0] + operationSymbol
+//                         operation.push(operationSymbol)
+//                         finishedOperation = false
+//
+//
+//                         break
+//                 }
+//
+//                 updateDisplay(operationDisplay, result, finishedOperation)
+//
+//
+//             }
+//         )
+//     })
+// }
+
+function updateOperation() {
+    //Si viene lleno con un valor anterior no se pone otro, que causaría repetición y que hubiera 4 valores en el array en vez de 3
+    // es para las operaciones encadenadas
+    // Add result data to the operation array if it's empty
+    // It is for chained operations (Example 4+4 = 8+4)
+    if (!operation[0]) {
+        operation.push(resultDisplay.value)
+    }
+}
+
+function getFirstValue() {
+    return parseFloat(resultDisplay.value)
+}
+
+function updateDisplay(stringOperation, result, addEqual = true) {
     if (addEqual) {
         stringOperation += " ="
         operation = [result]
     }
-    console.log(operation)
+
     previousDisplay.value = stringOperation
     resultDisplay.value = result
 }
@@ -70,26 +127,29 @@ function updateDisplay(stringOperation, result, addEqual=true) {
 function setBasicOperationsListener() {
     document.getElementById("=").addEventListener("click", () => {
         if (!Array.from(previousDisplay.value).some((letter) => letter === '=')) {
-            let [num1, operationSymbol, num2] = getOperationData()
-            let result;
-            switch (operationSymbol) {
-                case "+":
-                    result = CALCULATOR.aggregation(num1, num2)
-                    break
-                case "-":
-                    result = CALCULATOR.subtraction(num1, num2)
-                    break
-                case "*":
-                    result = CALCULATOR.multiplication(num1, num2)
-                    break
-                case "/":
-                    result = CALCULATOR.division(num1, num2)
-                    break
-            }
-            updateDisplay("" + num1 + operationSymbol + num2, result)
+            launchOperation()
         }
     })
+}
 
+function launchOperation() {
+    let [num1, operationSymbol, num2] = getOperationData()
+    let result;
+    switch (operationSymbol) {
+        case "+":
+            result = CALCULATOR.aggregation(num1, num2)
+            break
+        case "-":
+            result = CALCULATOR.subtraction(num1, num2)
+            break
+        case "*":
+            result = CALCULATOR.multiplication(num1, num2)
+            break
+        case "/":
+            result = CALCULATOR.division(num1, num2)
+            break
+    }
+    updateDisplay("" + num1 + operationSymbol + num2, result)
 }
 
 function getOperationData() {
@@ -98,7 +158,6 @@ function getOperationData() {
     return [parseFloat(operation[0]), operation[1], parseFloat(operation[2])]
 }
 
-// Inicio funciones de limpieza
 function setCleanListeners() {
     Array.from(document.getElementsByClassName("clean")).forEach((element) => {
         element.addEventListener("click", (e) => {
@@ -129,8 +188,6 @@ function clearAll() {
     operation = []
 }
 
-
-// Fin funciones de limpieza
 function setChangeSymbolListener() {
     document.getElementById("+/-").addEventListener("click", () => {
         cleanPreviousDisplay()
@@ -163,7 +220,8 @@ function setNumbersListeners() {
 
 function cleanPreviousDisplay() {
     let previous = previousDisplay.value
-    if (Array.from(previous).some((letter) => Array.from(availableOperations).some((symbol) => symbol === letter))) {
+    //if (Array.from(previous).some((letter) => Array.from(availableOperations).some((symbol) => symbol === letter))) {
+    if (Array.from(previous).some((letter) => '=' === letter)) {
         previousDisplay.value = ''
         operation = []
         resultDisplay.value = '0'
