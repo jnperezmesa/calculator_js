5ef77112d954013d886d0a44b2b7a9d245e4b055
Javier Nicolás Pérez Mesa <javier@perezmesa.com>
2024-06-02 17:27:57 +0200

    Updated the ui logic to make it more readable.

--- scripts/ui.js
+++ scripts/ui.js
@@ -5,6 +5,7 @@ const CALCULATOR = new Calculator()
 const PREVIOUS = "previous"
 const RESULT = "result"
 const BASIC_OPERATIONS = ['+', '-', '*', '/']
+const INITIAL_VALUE = 0
 
 
 // ============ Variables ============
@@ -17,11 +18,10 @@ let resultDisplay = document.getElementById(RESULT)
 // ============ Functions ============
 function initializeCalculator() {
     setCleanButtonListeners()
-
-    setBasicOperationsListener()
-    setOperationListeners()
     setChangeSymbolListener()
     setNumbersListeners()
+    setBasicOperationListener()
+    setOperationListeners()
 }
 
 function setCleanButtonListeners() {
@@ -32,136 +32,19 @@ function setCleanButtonListeners() {
     });
 }
 
-function setBasicOperationListener() {
-    document.getElementById("=").addEventListener("click", () => {
-        if (!previousDisplay.value.includes('=')) {
-            const [visualOperation, result] = executeOperation();
-            updateDisplay(visualOperation, result);
-        }
-    });
-}
-
-function setOperationListeners() {
-    Array.from(document.getElementsByClassName("operation")).forEach((element) => {
-        element.addEventListener("click", (e) => {
-                updateOperation()
-                let num1 = getFirstValue()
-                let result
-                let operationDisplay
-                let operationSymbol = e.target.id
-                let isConcatOperation = currentOperation.length === 2 && BASIC_OPERATIONS.includes(operationSymbol)
-
-                if (isConcatOperation) {
-                    const [_, result] = executeOperation()
-                    updateDisplay(result + operationSymbol, 0)
-                    currentOperation.push(operationSymbol)
-                } else {
-                    switch (operationSymbol) {
-                        case "√":
-                            result = CALCULATOR.squareRoot(num1)
-                            operationDisplay = "√" + num1 + ' ='
-                            break
-
-                        case "e":
-                            result = CALCULATOR.exponential(num1)
-                            operationDisplay = "e^" + num1 + ' ='
-                            break
-
-                        default:
-                            result = 0
-                            operationDisplay = currentOperation[0] + operationSymbol
-                            currentOperation.push(operationSymbol)
-                            break
-                    }
-
-                    if (!BASIC_OPERATIONS.includes(operationSymbol) && BASIC_OPERATIONS.includes(currentOperation[1])) {
-                        resultDisplay.value = result
-                        let savedOperation = [currentOperation[0], currentOperation[1]]
-
-                        let [_, result2] = executeOperation()
-                        result = result2
-                        operationDisplay = savedOperation[0] + savedOperation[1] + operationDisplay
-                    } else if (result) {
-                        currentOperation = [result]
-                    }
-
-                    if (!memoryResult) {
-                        memoryResult = num1
-                    }
-                    updateDisplay(operationDisplay, result)
-                }
-            }
-        )
-    })
-}
-
-function updateOperation() {
-    // Add result data to the operation array if it's empty
-    // It is for chained operations (Example 4+4 = 8+4)
-    if (!currentOperation[0]) {
-        currentOperation.push(resultDisplay.value)
-    }
-}
-
-function getFirstValue() {
-    return parseFloat(resultDisplay.value)
-}
-
-
-function executeOperation() {
-    const [num1, operationSymbol, num2] = getOperationData();
-    let result;
-
-    switch (operationSymbol) {
-        case "+": result = CALCULATOR.aggregation(num1, num2); break;
-        case "-": result = CALCULATOR.subtraction(num1, num2); break;
-        case "*": result = CALCULATOR.multiplication(num1, num2); break;
-        case "/": result = CALCULATOR.division(num1, num2); break;
-    }
-
-    memoryResult = result;
-    currentOperation = [result];
-
-    const visualOperation = `${num1}${operationSymbol}${num2} =`;
-    return [visualOperation, result];
-}
-
-function getOperationData() {
-    // [num1, operationSymbol, num2]
-    currentOperation.push(resultDisplay.value)
-    return [parseFloat(currentOperation[0]), currentOperation[1], parseFloat(currentOperation[2])]
-}
-
-function updateDisplay(previousOperation, result) {
-    previousDisplay.value = previousOperation
-    resultDisplay.value = result
-}
-
-
-function setBasicOperationsListener() {
-    document.getElementById("=").addEventListener("click", () => {
-        if (!Array.from(previousDisplay.value).some((letter) => letter === '=')) {
-            const [visualOperation, result] = executeOperation()
-            updateDisplay(visualOperation, result)
-        }
-    })
+function clearAll() {
+    updateDisplay('', INITIAL_VALUE);
+    currentOperation = [];
+    memoryResult = INITIAL_VALUE;
 }
 
 function removeLast() {
-    let result = resultDisplay.value
-    previousDisplay.value = '';
+    let result = resultDisplay.value;
     if (result.length === 1) {
-        resultDisplay.value = 0
+        updateDisplay('', INITIAL_VALUE);
     } else {
-        resultDisplay.value = result.slice(0, result.length - 1)
+        updateDisplay('', result.slice(0, result.length - 1));
     }
-}
-
-function clearAll() {
-    previousDisplay.value = ''
-    resultDisplay.value = '0'
-    currentOperation = []
-    memoryResult = 0
 }
 
 function setChangeSymbolListener() {
@@ -200,13 +83,121 @@ function setNumbersListeners() {
 function cleanDisplay() {
     let previous = previousDisplay.value
     if (Array.from(previous).some((letter) => '=' === letter)) {
-        memoryResult = ''
+        memoryResult = 0
         previousDisplay.value = ''
         currentOperation = []
         resultDisplay.value = '0'
     }
 }
 
+function setBasicOperationListener() {
+    document.getElementById("=").addEventListener("click", () => {
+        if (!previousDisplay.value.includes('=')) {
+            const [visualOperation, result] = executeOperation();
+            updateDisplay(visualOperation, result);
+        }
+    });
+}
+
+function setOperationListeners() {
+    Array.from(document.getElementsByClassName("operation")).forEach((element) => {
+        element.addEventListener("click", handleOperationClick);
+    });
+}
+
+function handleOperationClick(e) {
+    updateOperation();
+    let num1 = getFirstValue();
+    let operationSymbol = e.target.id;
+    let isConcatOperation = currentOperation.length === 2 && BASIC_OPERATIONS.includes(operationSymbol);
+
+    if (isConcatOperation) {
+        handleConcatOperation(operationSymbol);
+    } else {
+        handleSpecialOperation(num1, operationSymbol);
+    }
+}
+
+function getFirstValue() {
+    return parseFloat(resultDisplay.value)
+}
+
+function handleConcatOperation(operationSymbol) {
+    const [_, result] = executeOperation();
+    updateDisplay(result + operationSymbol, 0);
+    currentOperation.push(operationSymbol);
+}
+
+function handleSpecialOperation(num1, operationSymbol) {
+    let result;
+    let operationDisplay;
+
+    if (operationSymbol === "√") {
+        result = CALCULATOR.squareRoot(num1);
+        operationDisplay = `√${num1} =`;
+    } else if (operationSymbol === "e") {
+        result = CALCULATOR.exponential(num1);
+        operationDisplay = `e^${num1} =`;
+    } else {
+        result = 0;
+        operationDisplay = `${currentOperation[0]}${operationSymbol}`;
+        currentOperation.push(operationSymbol);
+    }
+
+    handleChainedOperation(result, operationDisplay, operationSymbol);
+}
+
+function handleChainedOperation(result, operationDisplay, operationSymbol) {
+    if (!BASIC_OPERATIONS.includes(operationSymbol) && BASIC_OPERATIONS.includes(currentOperation[1])) {
+        resultDisplay.value = result;
+        let savedOperation = [currentOperation[0], currentOperation[1]];
+
+        let [_, result2] = executeOperation();
+        result = result2;
+        operationDisplay = savedOperation[0] + savedOperation[1] + operationDisplay;
+    } else if (result) {
+        currentOperation = [result];
+    }
+    updateDisplay(operationDisplay, result);
+}
+
+function updateDisplay(previousOperation, result) {
+    memoryResult = result;
+    previousDisplay.value = previousOperation;
+    resultDisplay.value = result;
+}
+
+function updateOperation() {
+    // Add result data to the operation array if it's empty
+    // It is for chained operations (Example 4+4 = 8+4)
+    if (!currentOperation[0]) {
+        currentOperation.push(resultDisplay.value)
+    }
+}
+
+
+function executeOperation() {
+    const [num1, operationSymbol, num2] = getOperationData();
+    let result;
+
+    switch (operationSymbol) {
+        case "+": result = CALCULATOR.aggregation(num1, num2); break;
+        case "-": result = CALCULATOR.subtraction(num1, num2); break;
+        case "*": result = CALCULATOR.multiplication(num1, num2); break;
+        case "/": result = CALCULATOR.division(num1, num2); break;
+    }
+
+    currentOperation = [result];
+
+    const visualOperation = `${num1}${operationSymbol}${num2} =`;
+    return [visualOperation, result];
+}
+
+function getOperationData() {
+    // [num1, operationSymbol, num2]
+    currentOperation.push(resultDisplay.value)
+    return [parseFloat(currentOperation[0]), currentOperation[1], parseFloat(currentOperation[2])]
+}
 
 // ============ Initialize ============
 initializeCalculator()
